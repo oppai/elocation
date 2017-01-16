628040fa0a1afa229870dcb4c27321a6b1db07fa
hiroaki.murayama <hotsoup.h@gmail.com>
2017-01-16 15:32:42 +0900

    Fix package infomation

--- src/Elocation.elm
+++ src/Elocation.elm
@@ -1,6 +1,17 @@
 module Elocation exposing (..)
 
+{-| Library for getting URL on browser
+
+# Get URL
+@docs urlString
+
+-}
+
 import Native.Elocation
 
+-- Get URL
+
+{-| Get url by document.location
+-}
 urlString : String
 urlString = Native.Elocation.getUrlString ()
